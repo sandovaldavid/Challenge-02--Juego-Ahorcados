46967244bbb6631495c01108990d6ff077525620
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2022-06-08 08:43:42 -0500

    Elimine este js, porque lo implemente en palabra_adivinar.js

--- js/dibujo.js
+++
@@ -1,8 +0,0 @@
-var pantalla_dibujo = document.querySelector("#dibujo");
-var pincel = pantalla_dibujo.getContext("2d");
-    pincel.strokeStyle = "#0A3871";
-    pincel.beginPath()
-    pincel.moveTo(0, 350);
-    pincel.lineWidth = 4,5;
-    pincel.lineTo(300, 350);
-    pincel.stroke();
