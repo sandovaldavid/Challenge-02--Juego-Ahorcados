e705d61f9c55e50497beb3f17386ebe4a86992a1
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-26 23:24:36 -0500

    ✨ Enhance palabra addition with improved validation and add cancel functionality in palabras_nuevas.js

--- js/palabras_nuevas.js
+++ js/palabras_nuevas.js
@@ -1,13 +1,21 @@
 var b_guardar = document.querySelector("#b_guardar");
+var b_cancelar = document.querySelector("#b_cancelar");
 
 b_guardar.addEventListener("click", function () {
     var texto = document.getElementById("texto").value;
     var palabras = texto.split("\n").filter(palabra => {
+        palabra = palabra.trim().toUpperCase();
         return palabra.length > 0 &&
             palabra.length <= 8 &&
-            /^[A-Za-z]+$/.test(palabra);
+            /^[A-Z]+$/.test(palabra);
     });
 
-    palabras.forEach(palabra => palabras_predeterminadas.push(palabra));
-    window.location.href = 'juego.html';
+    if (palabras.length > 0) {
+        palabras.forEach(palabra => palabras_predeterminadas.push(palabra));
+        window.location.href = 'juego.html';
+    }
+});
+
+b_cancelar.addEventListener("click", function () {
+    window.location.href = 'index.html';
 });
