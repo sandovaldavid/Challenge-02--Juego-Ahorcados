9ff29900b60c34fed3bdc304d7d2bd77f0cf543f
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-26 23:13:14 -0500

    ✨ Improve input validation and streamline palabra addition in palabras_nuevas.js

--- js/palabras_nuevas.js
+++ js/palabras_nuevas.js
@@ -1,18 +1,13 @@
-
 var b_guardar = document.querySelector("#b_guardar");
 
-b_guardar.addEventListener("click", function(){
-    var nuevas_palabras =[];
+b_guardar.addEventListener("click", function () {
     var texto = document.getElementById("texto").value;
-    nuevas_palabras = texto.split("\n");   
-    for(var i=0 ; i<nuevas_palabras.length ; i++){
-        if(nuevas_palabras[i]!=""){
-            palabras_predeterminadas.push(nuevas_palabras[i]);
-            console.log(palabras_predeterminadas);
-        }
-    }
-    // funcion crear elementos de mi pagina juego.html
+    var palabras = texto.split("\n").filter(palabra => {
+        return palabra.length > 0 &&
+            palabra.length <= 8 &&
+            /^[A-Za-z]+$/.test(palabra);
+    });
+
+    palabras.forEach(palabra => palabras_predeterminadas.push(palabra));
+    window.location.href = 'juego.html';
 });
-
-
-
